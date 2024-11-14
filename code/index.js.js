8470ce8a38a9114a1138d51f87508b13f0d8d6c7
Ghaith Othmani <mekluless@gmail.com>
2024-11-14 05:39:03 +0100

    fixed routing

--- code/index.js.js
+++ code/index.js.js
@@ -4,61 +4,71 @@ const cookieParser = require('cookie-parser');
 const compression = require('compression');
 const helmet = require('helmet');
 
-
-
 const app = express();
-const port=3000;
+const port = 3000;
+
+
+
 
 //middleware
 app.use(compression());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
-//app.use('/pages', express.static(''));
-app.use(express.urlencoded({ extended: true })); 
-
 
 //routes
+const authRouter = express.Router();
+const bungalowRouter = express.Router();
+const rentalRouter = express.Router();
 
-
-
-app.get('/api/', (req, res) => {
+// Auth routes
+authRouter.get('/', (req, res) => {
     res.redirect('http://localhost/api/login');
 });
-app.post('/api/login', (req, res) => {
+authRouter.post('/login', (req, res) => {
     res.send('Method not implemented yet!');
 });
-app.post('/api/signup', (req, res) => {
+authRouter.post('/signup', (req, res) => {
     res.send('Method not implemented yet!');
 });
-app.post('/api/logout', (req, res) => {
+authRouter.post('/logout', (req, res) => {
     res.send('Method not implemented yet!');
 });
-app.get('/api/bungalows', (req, res) => {
+
+// Bungalows routes
+bungalowRouter.get('/', (req, res) => {
     res.send('Method not implemented yet!');
 });
-app.get('/api/bungalows/{id}', (req, res) => {
+bungalowRouter.get('/:id', (req, res) => {
     res.send('Method not implemented yet!');
 });
-app.post('/api/bungalows/{id}/rent', (req, res) => {
+bungalowRouter.post('/:id/rent', (req, res) => {
     res.send('Method not implemented yet!');
 });
-app.post('/api/bungalows/{id}/geo', (req, res) => {
+bungalowRouter.post('/:id/geo', (req, res) => {
     res.send('Method not implemented yet!');
 });
-app.get('/api/rentals/all', (req, res) => {
+
+rentalRouter.get('/all', (req, res) => {
     res.send('Method not implemented yet!');
 });
-app.delete('/api/rentals', (req, res) => {
+rentalRouter.delete('/', (req, res) => {
     res.send('Method not implemented yet!');
+});
+
+// Register routers with base paths
+app.use('/api', authRouter);
+app.use('/api/bungalows', bungalowRouter);
+app.use('/api/rentals', rentalRouter);
+
+// Server running
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
 });
 
 
 
 
 
-//server running
-app.listen(port,() =>{
-    console.log(`server running at http://localhost:${port}/`);
-    }
-);
+
+
